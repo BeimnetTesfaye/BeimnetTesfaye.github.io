0720401a46759b81edc10dcd1c2639f63fdd666f
Beimnet Tesfaye <chuni.yeab1@gmail.com>
2021-03-22 20:22:53 -0500

    Weel1Day1HW

---
+++ Cs_303/labW1D1/app.js
@@ -0,0 +1,48 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+function pow(x, n) {
+	return x ** n;
+}
+
+function add2(n1, n2) {
+	return n1 + n2;
+}
+
+function add(...args) {
+	let count = arguments.length;
+	let sum = 0;
+	for (let i=0; i<count; i++) {
+		sum += arguments[i];		
+	}
+	
+	return sum;
+}
+
+/*
+	Input: grades is an array of integers
+	Output: compute average of grades and report letter grade as follows:
+	90 - 100 A
+	80 - 89  B
+	70 - 79  C
+	60 - 69  D
+	0 - 59   NC
+*/
+function computeGrade(grades) {
+	let sum=0;
+	let average;
+	let lettergrade;
+	for(let i=0;i<grades.lenght;i++){
+	sum=sum+grades[i];
+		}
+		average=sum/grades.length;
+	if(average>=90)lettergrade="A";
+	else if(average>=80)lettergrade="B";
+	else if(average>=70)lettergrade="C";
+	else if(average>=60)lettergrade="D";
+	else lettergrade="NC";
+
+	return lettergrade;
+}
+

8f7eac8520b840ab4e6be0e1db95dbe9cd2e2461
Beimnet Tesfaye <chuni.yeab1@gmail.com>
2021-03-25 14:40:54 -0500

    W1D3HW

--- Cs_303/labW1D3/app.js
+++ Cs_303/labW1D3/app.js
@@ -1,3 +1,7 @@
+function load() {
+	let id = document.getElementById("result");
+	id.innerHTML = "Lab W1D3";
+}
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 "use strict";
@@ -13,30 +17,35 @@ Creates an object that contains the following information from the "this" object
 }	
 */
 function analyzer() {
-	let output = {};
-	let keysArray = Object.keys(this);
-	let valueArray = Object.values(this);
-	
-	output.numProperties = keysArray.length;
-  
-	let countNum = 0;
-	for (let each of keysArray) {
-	  
-	  if (each.length < 3) countNum++;
-	}
-	output.cntShortName = countNum;
-  
-	let countObj = 0;
-	for (let each of valueArray) {
-	  if (typeof each === "object") {
-		countObj++;
-	  }
-	}
-	output.cntReference = countObj;
-  
-	return output;
-  }
+	//let person = new Person("bob", "usa", [100, 90]);
 
+	let outputObject = {};
+	let key = Object.keys(this);
+		
+	outputObject.numProperties=key.length;
+
+		let count = 0;
+		for (let elements of Object.keys(this)) {
+			
+			if (elements.length < 3)  count++;
+				
+			
+	}
+	outputObject.cntShortName = count;
+	// Let's count the values in the object
+		let countval = 0;
+		for (let element of Object.values(this)) {
+			if (typeof element==="object") {
+				countval++;
+			}
+		}
+	outputObject.cntReference=countval;
+	return outputObject;
+	}
+	
+
+
+analyzer()
 
 
 /* Constructor for a person object
@@ -48,21 +57,16 @@ function analyzer() {
 		computeGrade     // function that computes the grade from grades
 	}
 */
-
-
-	function Person(name, country, grades) {
-		this.name = name;
-		this.country = country;
-		this.grades = grades;
-		this.computeGrade = function () {
-		  let sum = 0;
-		  let arr = this.grades;
-		  for (let key of arr) {
-			sum += key;
-
-		  }
-		  let average = sum / arr.length;
-		  return average;
-		};
-	  }
-
+function Person(name, country, grades) {
+	this.name = name;
+	this.country = country;
+	this.grades = grades;
+	this.computeGrade = function() {
+		let sum = 0;
+		for (let score of grades){
+			sum += score;
+		}
+		return sum/grades.length;
+			
+	}
+}
